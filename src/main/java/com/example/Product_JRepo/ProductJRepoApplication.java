c55f38fbc9dd1b42307638ca16fb7c26af770da0
Vivek Vatsa <72134769+Vivz-cyber@users.noreply.github.com>
2025-05-27 06:45:38 +0530

    some commit

--- src/main/java/com/example/Product_JRepo/ProductJRepoApplication.java
+++ src/main/java/com/example/Product_JRepo/ProductJRepoApplication.java
@@ -2,12 +2,18 @@ package com.example.Product_JRepo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProductJRepoApplication {
+public class ProductJRepoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductJRepoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ProductJRepoApplication.class);
+	}
 }
